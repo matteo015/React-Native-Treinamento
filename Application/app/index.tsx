f529b967a8301293ea86b72da3c6e4e6d86b8b19
Mdev <matheusdias.2077@gmail.com>
2025-03-19 17:24:27 -0300

    feat: TYPESCRIPT - INTRO

--- Application/app/index.tsx
+++ Application/app/index.tsx
@@ -1,18 +1,24 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
+const styles = StyleSheet.create({
+    default_header: {
+    fontSize: 64,
+    lineHeight: 24,
+    padding: 20,
+    color: '#ECEDEE',
+    backgroundColor: '#151718',
+  },
+});
 
+export function HeaderText(text : string){
+  return (
+    <Text style={styles.default_header}>{text}</Text>
+  );
+}
 
 //Main?
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    HeaderText('BOA TARDE PESSOAL')
   );
 }
